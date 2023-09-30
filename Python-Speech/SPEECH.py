b1546124d0501ab08a5555eb5b61b03750d66bcb
abhi-061 <abhi22umachigi@gmail.com>
2023-09-30 19:41:53 +0530

    Upated Speech script with latest cersion

--- Python-Speech/SPEECH.py
+++ Python-Speech/SPEECH.py
@@ -1,49 +1,25 @@
-#yPDF2 is a free and open-source pure-python 
-# pip install PyPDF2
-# PDF library capable of splitting, merging, cropping, and transforming the pages of PDF files
-
-
-#pyttsx3 is a text-to-speech conversion library in Python
-
-
-#pip install pyttsx3
-
-
 import PyPDF2
-
 import pyttsx3
 
-pdfReader = PyPDF2.PdfFileReader(open('dummypdf.pdf', 'rb'))
+try:
+    pdfReader = PyPDF2.PdfReader(open('dummypdf.pdf', 'rb'))
 
-#######CASE 1 PDF READ
-# Initialize the Pyttsx3 engine
+    # Initialize the Pyttsx3 engine
+    speaker = pyttsx3.init()
 
-speaker = pyttsx3.init()
+    # Initialize the 'text' variable to accumulate text from all pages
+    text = ""
 
-for page_num in range(pdfReader.numPages):
-    text =  pdfReader.getPage(page_num).extractText()
-    speaker.say(text)
+    for page_num in range(len(pdfReader.pages)):
+        page = pdfReader.pages[page_num]
+        text += page.extract_text()
+        speaker.say(page.extract_text())
+        speaker.runAndWait()
+    speaker.stop()
+
+    # Save the accumulated text to an audio file
+    speaker.save_to_file(text, 'audio.mp3')
     speaker.runAndWait()
-speaker.stop()
 
-
-
-speaker.save_to_file(text, 'audio.mp3')
-
-
- 
-####CASE 2 READ A STRING ###########
-# Create a string
-string = "Lorem Ipsum is simply dummy text " \
-    + "of the printing and typesetting industry."
- 
-# Initialize the Pyttsx3 engine
-engine = pyttsx3.init()
- 
-# We can use file extension as mp3 and wav, both will work
-engine.save_to_file(string, 'speech.mp3')
- 
-# Wait until above command is not finished.
-engine.runAndWait()
-
-
+except Exception as e:
+    print("An error occurred:", str(e))
