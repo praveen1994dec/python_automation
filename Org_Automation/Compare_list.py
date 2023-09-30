8d4c16ea82f06c97603873f953c1b6069ab29c74
abhi-061 <abhi22umachigi@gmail.com>
2023-09-30 13:32:16 +0530

    Install requests module before running script

--- Org_Automation/Compare_list.py
+++ Org_Automation/Compare_list.py
@@ -9,8 +9,8 @@ def file_read():
         for i in list:
             k=i.split("_")
             list2.append(k[0])
-            #v1 / v300
-        #print(list2)
+            v1 / v300
+        print(list2)
     else:
            print("list is empty")
 list2=['V1', 'V2', 'V300', 'V300', 'V301','V301']
@@ -18,6 +18,7 @@ for i in range(len(list2)):
      for j in range(i+1,len(list2)): 
           if(list2[i]==list2[j]):
               print("SAME")
+
 
  
  
