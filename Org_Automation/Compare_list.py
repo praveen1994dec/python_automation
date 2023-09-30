057a59d90cd6416802550095ab53a34081667348
abhi-061 <abhi22umachigi@gmail.com>
2023-09-30 13:35:49 +0530

    Comparing two lists

--- Org_Automation/Compare_list.py
+++ Org_Automation/Compare_list.py
@@ -13,7 +13,7 @@ def file_read():
         print(list2)
     else:
            print("list is empty")
-list2=['V1', 'V2', 'V300', 'V300', 'V301','V301']
+list2=['V1', 'V2', 'V300', 'V300', 'V301','V3021']
 for i in range(len(list2)):   
      for j in range(i+1,len(list2)): 
           if(list2[i]==list2[j]):
